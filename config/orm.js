131dd9c6680de6849a4dd6fd392ba69addff0d7d
Phil Dugliss <phillipdugliss@gmail.com>
2019-06-18 14:08:57 -0500

    Boiler Plate Code added
    -Sever Connected
    -db created
    -Boiler Plate code added to
       -config
       - controllers
       - models

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+// var connection = require("../config/connection.js");
+
+// var orm = {
+
+// // selectAll:  function(){};
+
+
+// // insertOne: 
+
+// // updateOne:
+
+
+
+// }
+
+// module.exports = orm;
